98d160060e68c77cd3f1e9e061e0278ccab98c85
tanipa1 <tania35.291@gmail.com>
2023-05-26 15:52:58 +0600

    rating

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,12 +1,50 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { useEffect, useState } from "react";
+// rating
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
     return (
         <div>
             <SectionTitle
                 subHeading={"---What Our Clients Say---"}
                 heading={"TESTIMONIALS"}
             ></SectionTitle>
+            <div className="w-3/4 mx-auto my-20">
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+                    {
+                        reviews.map(review => <SwiperSlide
+                            key={review._id}
+                        >
+                            <div className="text-center w-3/4 mx-auto">
+                                <Rating
+                                className="mx-auto mb-9"
+                                    style={{ maxWidth: 180 }}
+                                    value={review.rating}
+                                    readOnly
+                                />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto mb-9" width="80" height="80" viewBox="0 0 512 512"><path fill="black" d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"/></svg>
+                                <p>{review.details}</p>
+                                <h3 className="text-[#D99904] text-xl uppercase">{review.name}</h3>
+                            </div>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+            </div>
         </div>
     );
 };
